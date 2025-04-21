90afefe4d999055636651804f19a2bb1c8bc93c1
Maurits <mcb.westbroek@gmail.com>
2025-04-21 19:30:06 +0100

    Respond 404 for invalid target

--- app/main.py
+++ app/main.py
@@ -1,17 +1,28 @@
 import socket  # noqa: F401
 
-CRLF = b"\r\n"
+CRLF = "\r\n"
+VALID_TARGETS = ["/"]
 
+def parse_request_line(req_line: str) -> list[str]:
+    return req_line.split(" ")
 
 def main():
-    print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    print("Started server")
     conn, addr = server_socket.accept() # wait for client
     with conn:
         print(f"Connected by {addr}")
-        resp = b"HTTP/1.1 200 OK\r\n\r\n"
-        conn.sendall(resp)
+        while True:
+            raw_data = conn.recv(1024)
+            data = raw_data.decode().split(CRLF)
+            req_line = data.pop(0)
+            method, target, version = req_line.split(" ")
+            if target in VALID_TARGETS:
+                resp = b"HTTP/1.1 200 OK\r\n\r\n"
+            else:
+                resp = b"HTTP/1.1 404 Not Found\r\n\r\n"
+            conn.sendall(resp)
+            
 
 
 if __name__ == "__main__":
