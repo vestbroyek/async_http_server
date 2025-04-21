06526739114d12e191df15c14da81e65c92b9d90
Maurits <mcb.westbroek@gmail.com>
2025-04-21 19:12:24 +0100

    Implement simple 200 OK response

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 import socket  # noqa: F401
 
+CRLF = b"\r\n"
+
 
 def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+    with conn:
+        print(f"Connected by {addr}")
+        resp = b"HTTP/1.1 200 OK \r\n"
+        conn.sendall(resp)
 
 
 if __name__ == "__main__":
